5a03f47305b62744eac39cb2f546a7b77093b8b7
rshaw@neuralmagic.com <robertgshaw2@gmail.com>
2025-04-21 02:12:42 +0000

    fix bad merge
    Signed-off-by: rshaw@neuralmagic.com <robertgshaw2@gmail.com>

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -394,7 +394,7 @@ class LMCacheConnectorV1Impl:
         if attn_metadata is None:
             logger.warning(
                 "In connector.start_load_kv, but the attn_metadata is None")
-            # return
+            return
 
         # HACK: getting chunk size to correctly calculate retrieve mask
         assert self.lmcache_engine is not None
@@ -618,28 +618,9 @@ class LMCacheConnectorV1Impl:
         for finished_req_id in scheduler_output.finished_req_ids:
             self._request_trackers.pop(finished_req_id, None)
 
-<<<<<<< HEAD
-        for request_id in scheduler_output.new_KV_req_ids_to_send:
-            assert self.kv_role == "kv_producer"
-            request_tracker = self._request_trackers[request_id]
-
-            req_meta = ReqMeta.from_request_tracker(
-                request_tracker,
-                self._block_size,
-                self._lmcache_chunk_size,
-                load_spec=None,
-                skip_save=False,
-                discard_partial_chunks=self._discard_partial_chunks)
-            if req_meta is not None:
-                meta.add_request(req_meta)
-
-        for request in scheduler_output.scheduled_new_reqs:
-            # Right now, we only load KV for new requests
-=======
         for request in scheduler_output.scheduled_new_reqs:
             # NOTE(rob): all new reqs are added to tracker for both
             # producer and consumer side.
->>>>>>> async_pd
             load_spec = self.load_specs.pop(request.req_id, None)
             num_tokens_to_compute = request.num_computed_tokens + \
                     scheduler_output.num_scheduled_tokens[request.req_id]
@@ -647,17 +628,11 @@ class LMCacheConnectorV1Impl:
                 request, num_tokens_to_compute)
             self._request_trackers[request.req_id] = request_tracker
 
-<<<<<<< HEAD
-            # SKIP saving until explicitly asked to.
-            if self.kv_role == "kv_producer":
-                continue
-
-=======
             # NOTE(rob): scheduled new reqs need to be loaded on
             # the consumer side only.
             if self.kv_role == "kv_producer":
                 continue
->>>>>>> async_pd
+
             req_meta = ReqMeta.from_request_tracker(
                 request_tracker,
                 self._block_size,
@@ -668,29 +643,6 @@ class LMCacheConnectorV1Impl:
             if req_meta is not None:
                 meta.add_request(req_meta)
 
-<<<<<<< HEAD
-        # NOTE(rob): this is not needed since:
-        #   - a) we do not need to chunking (we send the KVs)
-        #           in one group after the request is done prefilling
-        #   - b) we do not need to load (on consumer side) once
-        #           we are in the decode phase
-        # NOTE(rob): this means we cannot get external blocks
-        # for resumed requests that were preempted.
-
-        # for request in scheduler_output.scheduled_cached_reqs:
-        #     request_tracker = self._request_trackers[request.req_id]
-        #     request_tracker.update(request)
-
-        #     req_meta = ReqMeta.from_request_tracker(
-        #         request_tracker,
-        #         self._block_size,
-        #         self._lmcache_chunk_size,
-        #         load_spec=None,
-        #         skip_save=force_skip_save,
-        #         discard_partial_chunks=self._discard_partial_chunks)
-        #     if req_meta is not None:
-        #         meta.add_request(req_meta)
-=======
         # NOTE(rob): producer side sends the KVs.
         for request_id in scheduler_output.new_KV_req_ids_to_send:
             assert self.kv_role == "kv_producer"
@@ -705,7 +657,6 @@ class LMCacheConnectorV1Impl:
                 discard_partial_chunks=self._discard_partial_chunks)
             if req_meta is not None:
                 meta.add_request(req_meta)
->>>>>>> async_pd
 
         # NOTE(rob): this is not needed since:
         #   - a) we do not need to chunking (we send the KVs)
